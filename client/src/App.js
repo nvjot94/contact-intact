14922508ba0ecf6270997fab99209b11b79065f2
nvjot94 <nvjot94@gmail.com>
2020-04-06 02:46:18 -0400

    alert context

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import About from "./components/pages/about";
 import Navbar from "./components/layout/navbar";
 import ContactState from "./context/contact/contactState";
 import AuthState from "./context/auth/AuthState";
+import AlertState from "./context/alert/AlertState";
 import Register from "./components/auth/register";
 import Login from "./components/auth/login";
 import "./App.css";
@@ -12,19 +13,21 @@ const App = () => {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <Fragment>
-            <Navbar />
-            <div className="container">
-              <Switch>
-                <Route exact path="/" component={Home}></Route>
-                <Route exact path="/about" component={About}></Route>
-                <Route exact path="/register" component={Register}></Route>
-                <Route exact path="/login" component={Login}></Route>
-              </Switch>
-            </div>
-          </Fragment>
-        </Router>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className="container">
+                <Switch>
+                  <Route exact path="/" component={Home}></Route>
+                  <Route exact path="/about" component={About}></Route>
+                  <Route exact path="/register" component={Register}></Route>
+                  <Route exact path="/login" component={Login}></Route>
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
